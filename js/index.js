291a236ea8de7c97a4a1ca5f849d98f761802596
zachchen1227 <zachchen1227@gmail.com>
2025-10-17 10:12:55 +0800

    增加客戶評分卡

--- js/index.js
+++ js/index.js
@@ -51,22 +51,26 @@ class beanInfo {
  */
 class productInfo {
     // 私有欄位
-    #id = ""
+    #id = ""; // 英文id
     #countryName = ""; // 國家名稱 (中文)
     #terroir = "";     // 主要產區/莊園 (字串)
+    #productName = "";  // 產品名稱 (字串)
     #flavor = "";      // 風味描述 (字串)
     #price = [];       // 價格 (陣列)
 
     /**
+     * @param {string} id - id(英文)
      * @param {string} countryName - 國家名稱 (中文)
      * @param {string} terroir - 產區
+     * @param {string} productName - 產品名稱 (字串)
      * @param {string} flavor - 風味描述
-     * @param {string[]} price - 產區陣列
+     * @param {string[]} price - 價格 (陣列)
      */
-    constructor(id, countryName, terroir, flavor, price) {
+    constructor(id, countryName, terroir, productName, flavor, price) {
         this.#id = id;
         this.#countryName = countryName;
         this.#terroir = terroir;
+        this.#productName = productName;
         this.#flavor = flavor;
         this.#price = price;
     }
@@ -80,9 +84,14 @@ class productInfo {
         return this.#countryName;
     }
 
+    get productName() {
+        return this.#productName;
+    }
+
     get terroir() {
         return this.#terroir;
     }
+
 
     get flavor() {
         return this.#flavor;
@@ -94,6 +103,119 @@ class productInfo {
 }
 
 
+/**
+ * @class reviewInfo
+ * 顧客評論卡的資料結構類別
+ * 使用 Private 欄位 (#) 確保資料的封裝性 (Encapsulation)
+ */
+class reviewInfo {
+    // 私有欄位
+    #userId = "";      // 使用者帳號
+    #userName = "";    // 使用者名稱
+    #countryName = ""; // 國家名稱 (中文)
+    #terroir = "";     // 產區
+    #productName = "";  // 產品名稱
+    #scoreSet = new scoreSet(0, 0, 0, 0);    // 分數陣列，1-5顆星，共四個元素，分別代表酸感、甜感、苦感、風味
+    #flavor = "";      // 風味描述 (字串)
+
+
+    /**
+     * @param {string} userId - 使用者帳號
+     * @param {string} userName - 使用者名稱
+     * @param {string} countryName - 國家名稱 (中文)
+     * @param {string} terroir - 產區
+     * @param {string} productName - 產品名稱
+     * @param {number[]} scoreSet - 分數陣列
+     * @param {string} flavor - 風味描述
+     */
+    constructor(userId, userName, countryName, terroir, productName, scoreSet, flavor) {
+        this.#userId = userId;
+        this.#userName = userName;
+        this.#countryName = countryName;
+        this.#terroir = terroir;
+        this.#productName = productName;
+        this.#scoreSet = scoreSet;
+        this.#flavor = flavor;
+    }
+
+    // Public Getters (讀取器)
+    get userId() {
+        return this.#userId;
+    }
+
+    get userName() {
+        return this.#userName;
+    }
+
+    get countryName() {
+        return this.#countryName;
+    }
+
+    get terroir() {
+        return this.#terroir;
+    }
+
+    get productName() {
+        return this.#productName;
+    }
+
+    get scoreSet() {
+        return this.#scoreSet;
+    }
+
+    get flavor() {
+        return this.#flavor;
+    }
+}
+
+class scoreSet {
+    #sourness_score = 0;
+    #sweetness_score = 0;
+    #bitterness_score = 0;
+    #flavor_score = 0;
+
+    constructor(sourness_score, sweetness_score, bitterness_score, flavor_score) {
+        this.#sourness_score = sourness_score;
+        this.#sweetness_score = sweetness_score;
+        this.#bitterness_score = bitterness_score;
+        this.#flavor_score = flavor_score;
+    }
+
+
+    get sourness_score() {
+        return this.#sourness_score;
+    }
+
+    get sweetness_score() {
+        return this.#sweetness_score;
+    }
+
+    get bitterness_score() {
+        return this.#bitterness_score;
+    }
+
+    get flavor_score() {
+        return this.#flavor_score;
+    }
+
+    set sourness_score(newScore) {
+        this.#sourness_score = newScore;
+    }
+
+    set sweetness_score(newScore) {
+        this.#sweetness_score = newScore;
+    }
+
+    set bitterness_score(newScore) {
+        this.#bitterness_score = newScore;
+    }
+
+    set flavor_score(newScore) {
+        this.#flavor_score = newScore;
+    }
+}
+
+
 
 //==============================================
 // 區塊 2: 核心資料庫
@@ -101,9 +223,7 @@ class productInfo {
 
 /**
  * @const {Object.<string, beanInfo>} beanInfoList
- * 儲存所有咖啡豆產地資訊的物件。
- * Key 使用 國家名稱。
- * Value 為 beanInfo 類別的實例。
+ * 儲存所有咖啡豆產地資訊的陣列。
  */
 let beanInfoList = [
     new beanInfo("Ethiopia", "衣索比亞", ["耶加雪菲", "西達摩", "古吉"], "花香(如茉莉)、柑橘、莓果、茶感、層次豐富。"),
@@ -116,22 +236,41 @@ let beanInfoList = [
 ]
 
 /**
- * @const {Object.<string, beanInfo>} productInfoList
- * 儲存所有產品資訊的物件。
- * Key 使用 國家名稱+編號。
- * Value 為 beanInfo 類別的實例。
+ * @const {Object.<string, productInfo>} productInfoList
+ * 儲存所有產品資訊的陣列。
  */
 let productInfoList = [
-    new productInfo("Ethiopia", "衣索比亞", "耶加雪菲", "濃郁花香、清爽茶感", ["350", "680"]),
-    new productInfo("Ethiopia_1", "衣索比亞", "西達摩", "明亮果酸、水果甜感", ["400", "780"]),
-    new productInfo("Kenya", "肯亞", "尼耶里", "柑橘、濃郁巧克力", ["450", "880"]),
-    new productInfo("Kenya_1", "肯亞", "奇里尼亞加", "黑醋栗、紅莓果、花香", ["400", "690"]),
-    new productInfo("Guatemala", "瓜地馬拉 ", "薇薇特南果", "柑橘、莓果、花香", ["380", "750"]),
-    new productInfo("CostaRica", "哥斯大黎加", "塔拉珠", "柑橘、蜂蜜、花香", ["550", "1000"]),
-    new productInfo("Colombia", "哥倫比亞 ", "薇拉", "橘酸，焦糖、巧克力", ["680", "1280"]),
-    new productInfo("Brazil", "巴西", "喜拉朵", "紅蘋果、蜂蜜、花香", ["480", "920"]),
-    new productInfo("Brazil_1", "巴西", "米納斯吉拉斯", "甜感、堅果、巧克力", ["500", "970"]),
-    new productInfo("Indonesia", "印尼", "蘇門答臘", "煙草、藥草、黑巧克力。", ["700", "1350"])
+    new productInfo("Ethiopia", "衣索比亞", "耶加雪菲", "花神之春", "濃郁花香、清爽茶感", ["350", "680"]),
+    new productInfo("Ethiopia_1", "衣索比亞", "西達摩", "豐收大地", "明亮果酸、水果甜感", ["400", "780"]),
+    new productInfo("Kenya", "肯亞", "尼耶里", "曠野嘆息", "柑橘、濃郁巧克力", ["450", "880"]),
+    new productInfo("Kenya_1", "肯亞", "奇里尼亞加", "水果派對", "黑醋栗、紅莓果、花香", ["400", "690"]),
+    new productInfo("Guatemala", "瓜地馬拉 ", "薇薇特南果", "悠揚晨曦", "柑橘、莓果、花香", ["380", "750"]),
+    new productInfo("CostaRica", "哥斯大黎加", "塔拉珠", "小熊維尼", "柑橘、蜂蜜、花香", ["550", "1000"]),
+    new productInfo("Colombia", "哥倫比亞 ", "薇拉", "黑色跑車", "橘酸，焦糖、巧克力", ["680", "1280"]),
+    new productInfo("Brazil", "巴西", "喜拉朵", "哈囉你好", "紅蘋果、蜂蜜、花香", ["480", "920"]),
+    new productInfo("Brazil_1", "巴西", "米納斯吉拉斯", "火紅聖誕", "甜感、堅果、巧克力", ["500", "970"]),
+    new productInfo("Indonesia", "印尼", "蘇門答臘", "獵人小屋", "煙草、藥草、黑巧克力", ["700", "1350"])
+]
+
+
+/**
+ * @const {Object.<string, reviewInfo>} reviewInfoList
+ * 儲存所有顧客評論的陣列。
+ */
+let reviewInfoList = [
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 4, 3, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 2, 3, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 4, 2, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 4, 3, 2), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 4, 0, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 0, 3, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(0, 4, 3, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 1, 3, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 4, 1, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 4, 3, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 4, 1, 4), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 1, 3, 1), "濃郁花香、清爽茶感"),
+    new reviewInfo("iven0127", "伊凡周", "衣索比亞", "耶加雪菲", "花神之春", new scoreSet(3, 2, 2, 4), "濃郁花香、清爽茶感")
 ]
 
 
@@ -146,6 +285,7 @@ document.addEventListener('DOMContentLoaded', initializePage);
 function initializePage() {
     initializeBeaconEvents();
     initializeProductSwiper();
+    initializeReviewSwiper();
 }
 
 /**
@@ -208,32 +348,95 @@ function hideBeanAreaInfo() {
 // 初始化產品卡
 function initializeProductCard() {
 
-    let wrapperDiv = $(".swiper-wrapper");
-
-
+    let wrapperDiv = $(".productWrapper");
     for (let i = 0; i < productInfoList.length; i++) {
-        let slideDiv = $("<div class='card swiper-slide'></div>");
+        let slideDiv = $("<div class='productCard swiper-slide'></div>");
         let sub_img = $(`<img src=../images/product/product_${productInfoList[i].id}.jpg alt="Master's Brew">`);
-        let sub_h3 = $(`<h3>${productInfoList[i].countryName} | ${productInfoList[i].terroir}</h3>`);
-        let sub_p1 = $(`<p>${productInfoList[i].flavor}</p>`);
-        let sub_p2 = $(`<p class = "price"><strong>NT$${productInfoList[i].price[0]} ~ NT$${productInfoList[i].price[1]}</strong></p>`);
-        let sub_p3 = $("<p><button>加入購物車</button></p>");
-        slideDiv.append(sub_img, sub_h3, sub_p1, sub_p2, sub_p3);
+        let sub_countryName = $(`<h3>${productInfoList[i].countryName} | ${productInfoList[i].terroir}</h3>`);
+        let sub_productName = $(`<p><strong>${productInfoList[i].productName}</strong></p>`);
+        let sub_flavor = $(`<p>${productInfoList[i].flavor}</p>`);
+        let sub_price = $(`<p class = "price"><strong>NT$${productInfoList[i].price[0]} ~ NT$${productInfoList[i].price[1]}</strong></p>`);
+        let sub_button = $("<p><button>加入購物車</button></p>");
+        slideDiv.append(sub_img, sub_countryName, sub_productName, sub_flavor, sub_price, sub_button);
         wrapperDiv.append(slideDiv);
     }
+}
 
+// 初始化顧客評分卡
+function initializeReviewCard() {
 
+    let wrapperDiv = $(".reviewWrapper");
+    for (let i = 0; i < reviewInfoList.length; i++) {
+        let customerReview = reviewInfoList[i];
+        let slideDiv = $("<div class='reviewCard swiper-slide'></div>");
+        let sub_img = $('<img src=../images/reviewLogo.png alt="customer review">');
+        let sub_customerName = $(`<h3>${customerReview.userName}</h3>`);
+        let sub_location = $(`<p>${customerReview.countryName} | ${customerReview.terroir}</p>`);
+        let sub_productName = $(`<p>${customerReview.productName}</p>`);
+        let sub_scoreSet = getStarElement(customerReview.scoreSet);
+        let sub_flavor = $(`<p>${customerReview.flavor}</p>`);
+
+        slideDiv.append(
+            sub_img,
+            sub_customerName,
+            sub_location,
+            sub_productName,
+            sub_scoreSet,
+            sub_flavor);
+
+        wrapperDiv.append(slideDiv);
+    }
+}
+
+function getStarElement(scoreSet) {
+
+    let mySpan = [];
+
+    // 建立sourness 星星評分html
+    mySpan.push($(`<span class="starReview">酸味: </span>`))
+    mySpan.push(...createStarHtml(scoreSet.sourness_score));
+    mySpan.push($("<br>"));
+
+    // 建立sweetness 星星評分html
+    mySpan.push($(`<span class="starReview">甜味: </span>`))
+    mySpan.push(...createStarHtml(scoreSet.sweetness_score));
+    mySpan.push($("<br>"));
+
+    // 建立bitterness 星星評分html
+    mySpan.push($(`<span class="starReview">苦味: </span>`))
+    mySpan.push(...createStarHtml(scoreSet.bitterness_score));
+    mySpan.push($("<br>"));
+
+    // 建立flavor 星星評分html
+    mySpan.push($(`<span class="starReview">風味: </span>`))
+    mySpan.push(...createStarHtml(scoreSet.flavor_score));
+    return mySpan;
+}
+
+function createStarHtml(score) {
+    let sub_span = [];
+    let fullScore = 5;
+
+    for (let i = 0; i < score; i++) {
+        sub_span.push($(`<span class="fa fa-star checked"></span>`));
+    }
+
+    for (let i = 0; i < fullScore - score; i++) {
+        sub_span.push($(`<span class="fa fa-star"></span>`));
+    }
+
+    return sub_span;
 }
 
 
-// 初始化產品卡輪播系統
+// 初始化產品資訊卡輪播系統
 function initializeProductSwiper() {
 
-    // 初始化產品卡
+    // 初始化產品資訊卡
     initializeProductCard()
 
     // 初始化輪播系統
-    var swiper = new Swiper(".mySwiper", {
+    let swiper = new Swiper(".productSwiper", {
         slidesPerView: 'auto',
         grabCursor: true,
 
@@ -249,14 +452,69 @@ function initializeProductSwiper() {
         loop: false, // 確保是非循環模式
 
         pagination: {
-            el: ".swiper-pagination",
+            el: ".product-pagination",
             clickable: true,
             dynamicBullets: true,
         },
 
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: ".product-next",
+            prevEl: ".product-prev",
+        },
+
+        // Let's Make it Autoplay
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false
+        },
+        // Responsive
+        breakpoints: {
+            0: {
+                slidesPerView: 1
+            },
+            768: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 3
+            }
+        }
+    });
+}
+
+
+
+// 初始化顧客評分卡輪播系統
+function initializeReviewSwiper() {
+
+
+    initializeReviewCard();
+
+    // 初始化輪播系統
+    let swiper = new Swiper(".reviewSwiper", {
+        slidesPerView: 'auto',
+        grabCursor: true,
+
+        // 啟用居中模式
+        centeredSlides: true,
+
+        // ***關鍵設定：讓捲動邊界貼合起點和終點***
+        centeredSlidesBounds: true,
+
+        // 滑塊間距
+        spaceBetween: 30,
+
+        loop: false, // 確保是非循環模式
+
+        pagination: {
+            el: ".review-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+
+        navigation: {
+            nextEl: ".review-next",
+            prevEl: ".review-prev",
         },
 
         // Let's Make it Autoplay
